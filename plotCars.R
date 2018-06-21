57675f110901affb99225760f8aea2aaf531e79e
edeomano <emilydeomano@gmail.com>
2018-06-21 12:27:16 +1000

    Committing my first file in git

---
+++ plotCars.R
@@ -0,0 +1,16 @@
+
+# load data
+data(cars)
+str(cars)
+
+# load library
+library(tidyverse)
+
+# plot the data
+cars %>%
+  ggplot(aes(speed, dist)) +
+  geom_bar(stat = "identity") +
+  coord_flip()
+
+
+
